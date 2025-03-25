65a6e72cdc2b8f25cd3585974bc426610e11a015
kevinmel12 <melkevinmeledje@gmail.com>
2025-03-25 09:34:23 +0100

    added CORS package and CorsConfig class

---
+++ azurimmo/src/main/java/bts/sio/azurimmo/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package bts.sio.azurimmo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        // Autoriser les requêtes depuis votre frontend
+        config.addAllowedOrigin("http://localhost:3000");
+        
+        // Autoriser tous les headers et méthodes HTTP
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+}
