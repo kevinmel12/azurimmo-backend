d7e392e264b88e098b6f96989cf82ba1bf2f9179
kevinmel12 <melkevinmeledje@gmail.com>
2025-06-02 11:10:24 +0200

    WIP3

--- azurimmo/src/main/java/bts/sio/azurimmo/model/Associe.java
+++ azurimmo/src/main/java/bts/sio/azurimmo/model/Associe.java
@@ -1,55 +1,21 @@
 package bts.sio.azurimmo.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Data
 @Entity
-@Table(name = "associe")
 public class Associe {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
 
-    @Column(name = "nom")
-    private String nom;
+	    @Column(name = "nom")
+	    private String nom;
 
-    @Column(name = "prenom")
-    private String prenom;
-
-    @OneToMany(mappedBy = "associe", fetch = FetchType.LAZY)
-    private List<Contrat> contrats;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public List<Contrat> getContrats() {
-        return contrats;
-    }
-
-    public void setContrats(List<Contrat> contrats) {
-        this.contrats = contrats;
-    }
+	    @Column(name = "prenom")
+	    private String prenom;
 }
